75766c26cc1784be42d9d44e30513099008beee4
AATAKSH <akshata360@gmail.com>
2024-07-07 22:32:17 +0530

    commit assignment 4 first change

---
+++ src/main/java/akshata/demo/akshata_dev/AkshataDevApplication.java
@@ -0,0 +1,13 @@
+package akshata.demo.akshata_dev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkshataDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkshataDevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/akshata/demo/akshata_dev/AkshataDevApplicationTests.java
@@ -0,0 +1,13 @@
+package akshata.demo.akshata_dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AkshataDevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
